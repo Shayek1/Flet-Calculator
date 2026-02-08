03dfbfbe2820fd1b6d5c22af18b4a13f87720ac1
Shayek Ahmed <shayek.a@outlook.com>
2026-02-08 15:03:03 +0000

    add container around app

--- src/calculator.py
+++ src/calculator.py
@@ -3,39 +3,47 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Calculator"
-    result = ft.Text(value="0")
+    result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
 
     page.add(
-        ft.Row(controls=[result]),
-        ft.Row(controls=[ft.Button("AC"),
-                         ft.Button("+/-"),
-                         ft.Button("%"),
-                         ft.Button("/"),
-                         ]
-               ),
-        ft.Row(controls=[ft.Button("7"),
-                         ft.Button("8"),
-                         ft.Button("9"),
-                         ft.Button("*"),
-                         ]
-               ),
-        ft.Row(controls=[ft.Button("4"),
-                         ft.Button("5"),
-                         ft.Button("6"),
-                         ft.Button("-"),
-                         ]
-               ),
-        ft.Row(controls=[ft.Button("1"),
-                         ft.Button("2"),
-                         ft.Button("3"),
-                         ft.Button("+"),
-                         ]
-               ),
-        ft.Row(controls=[ft.Button("0"),
-                         ft.Button("."),
-                         ft.Button("="),
-                         ]
-               ),
+        ft.Container(
+            width=350,
+            bgcolor=ft.Colors.BLACK,
+            border_radius=ft.BorderRadius.all(20),
+            padding=20,
+            content=ft.Column(
+                controls =[ft.Row(controls=[result]),
+                           ft.Row(controls=[ft.Button("AC"),
+                                            ft.Button("+/-"),
+                                            ft.Button("%"),
+                                            ft.Button("/"),
+                                            ]
+                                  ),
+                           ft.Row(controls=[ft.Button("7"),
+                                            ft.Button("8"),
+                                            ft.Button("9"),
+                                            ft.Button("*"),
+                                            ]
+                                  ),
+                           ft.Row(controls=[ft.Button("4"),
+                                            ft.Button("5"),
+                                            ft.Button("6"),
+                                            ft.Button("-"),
+                                            ]
+                                  ),
+                           ft.Row(controls=[ft.Button("1"),
+                                            ft.Button("2"),
+                                            ft.Button("3"),
+                                            ft.Button("+"),
+                                            ]
+                                  ),
+                           ft.Row(controls=[ft.Button("0"),
+                                            ft.Button("."),
+                                            ft.Button("="),
+                                            ]
+                                  ),],
+            )
+        ),
     )
 
 
