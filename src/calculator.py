11e2fb0fa2e26091a763e865db30bdf76a783ea2
Shayek Ahmed <shayek.a@outlook.com>
2026-02-09 11:51:31 +0000

    create action button functions

--- src/calculator.py
+++ src/calculator.py
@@ -25,6 +25,83 @@ class ACButton(CalculatorButton):
     bgcolor: ft.Colors = ft.Colors.RED
 
 
+def clicked(self, e):
+    info = e.control.content
+    print(f"Button clicked with info = {info}")
+    #to reset calulcator if something goes wrong or AC is pressed
+    if self.result.value == "Error" or info == "AC":
+        self.result.value = "0"
+        self.reset()
+
+    #distinguishing between the numbers and the operators
+    elif info in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+        if self.result.value == "0" or self.new_operand:
+            self.result.value = info
+            self.new_operand = False
+        else:
+            self.result.value = self.result.value + info
+
+    elif info in ("+", "-", "*", "/"):
+        self.result.value = self.calculate(
+            self.operand1, info(self.result.value), self.operator
+        )
+        self.operator = info
+        if self.result.value == "Error":
+            self.operand1 = "0"
+        else:
+            self.operand1 = info(self.result.value)
+        self.new_operand = True
+
+    elif info in ("="):
+        self.result.value = self.calculate(
+            self.operand1, float(self.result.value), self.operator
+        )
+        self.reset()
+
+    elif info in ("%"):
+        self.result.value = float(self.result.value) / 100
+        self.reset()
+
+    elif info in ("+/-"):
+        if float(self.result.value) > 0:
+            self.result.value = "-" + str(self.result.value)
+
+        elif info(self.result.value) < 0:
+            self.result.value = str(
+                self.format_number(abs(float(self.result.value)))
+            )
+
+    self.update()
+
+    #formatting the value into a num if the value equals to an integer
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
+
+
 
 class Caclulator(ft.Container):
     def init(self):
