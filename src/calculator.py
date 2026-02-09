3b153f6cfb55e33ebf0088608cb43f960b9177b4
Shayek Ahmed <shayek.a@outlook.com>
2026-02-09 11:38:15 +0000

    made calculator into a reusable class

--- src/calculator.py
+++ src/calculator.py
@@ -1,45 +1,45 @@
 import flet as ft
 
 
-def main(page: ft.Page):
-    page.title = "Calculator"
-    result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
 
-    #This is a general format for all the buttons
-    @ft.control
-    class CalculatorButton(ft.Button):
-        expand: int = 1
+#This is a general format for all the buttons
+@ft.control
+class CalculatorButton(ft.Button):
+    expand: int = 1
 
 
-    @ft.control
-    class DigitalButton(CalculatorButton):
-      color: ft.Colors = ft.Colors.BLACK
-      bgcolor: ft.Colors = ft.Colors.WHITE
+@ft.control
+class DigitalButton(CalculatorButton):
+    color: ft.Colors = ft.Colors.BLACK
+    bgcolor: ft.Colors = ft.Colors.WHITE
 
 
-    @ft.control
-    class ActionButton(CalculatorButton):
-        color: ft.Colors = ft.Colors.BLACK
-        bgcolor: ft.Colors = ft.Colors.GREY
+@ft.control
+class ActionButton(CalculatorButton):
+    color: ft.Colors = ft.Colors.BLACK
+    bgcolor: ft.Colors = ft.Colors.GREY
 
-    @ft.control
-    class ACButton(CalculatorButton):
-       color: ft.Colors = ft.Colors.BLACK
-       bgcolor: ft.Colors = ft.Colors.RED
+@ft.control
+class ACButton(CalculatorButton):
+    color: ft.Colors = ft.Colors.BLACK
+    bgcolor: ft.Colors = ft.Colors.RED
 
 
 
+class Caclulator(ft.Container):
+    def init(self):
+        self.width = 350
+        self.bgcolor = ft.Colors.BLUE_GREY
+        self.border_radius = ft.BorderRadius.all(20)
+        self.padding = 20
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
 
-    page.add(
-        ft.Container(
-            width=350,
-            bgcolor=ft.Colors.BLUE_GREY,
-            border_radius=ft.BorderRadius.all(20),
-            padding=20,
-            content=ft.Column(
-                controls =[ft.Row(controls=[result],
-                                  alignment=ft.MainAxisAlignment.END),
-
+        self.content = ft.Column(
+                controls =
+                [ft.Row(
+                    controls=[self.result],
+                    alignment=ft.MainAxisAlignment.END
+                ),
                            ft.Row(controls=[ACButton(content="AC"),
                                             ActionButton(content="+/-"),
                                             ActionButton(content="%"),
@@ -68,10 +68,18 @@ def main(page: ft.Page):
                                             DigitalButton(content="."),
                                             ActionButton(content="="),
                                             ]
-                                  ),],
-            )
-        ),
-    )
+                                  ),
+                ],
+
+        )
+
+
+def main(page: ft.Page):
+    page.title = "Calculator"
+
+    calculator = Caclulator()
+
+    page.add(calculator)
 
 
 if __name__ == "__main__":
